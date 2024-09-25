3f9cff0f1ea9d7ef41ac92b18af112c085d2f03b
oshahadat5030 <shahadat@ksu.edu>
2024-09-25 13:23:31 -0500

    Update CoordinateCalculator.py

--- CoordinateCalculator.py
+++ CoordinateCalculator.py
@@ -1,6 +1,6 @@
 ##-------------------------------------------------------------------------------------
 ## Name:  CoordinateCalculator.py
 ## Description:  Converts geographic coordinates in DDMMSS format to DD
-## Created:  June 15, 2023
+## Created:  9/25/24
 ## Author:  Shawn Hutchinson (shutch@ksu.edu)
 ##-------------------------------------------------------------------------------------
