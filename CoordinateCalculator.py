08ed1791fa7b544a5b89b4f94309893a75cc924e
oshahadat5030 <shahadat@ksu.edu>
2024-11-20 18:47:07 -0600

    Update CoordinateCalculator.py

--- CoordinateCalculator.py
+++ CoordinateCalculator.py
@@ -1,6 +1,26 @@
 ##-------------------------------------------------------------------------------------
 ## Name:  CoordinateCalculator.py
 ## Description:  Converts geographic coordinates in DDMMSS format to DD
-## Created:  June 15, 2023
-## Author:  Shawn Hutchinson (shutch@ksu.edu)
+## Created:  11/20/2024
+## Author:  Shahadat Hossain
 ##-------------------------------------------------------------------------------------
+
+#Step 1 - Use the Python input function to prompt a user to enter a geographic coordinate in DDMMSS format (e.g., 391120).
+x= input('Please enter a coordinate value in DDMMSS format:' )
+
+#Step 2 - Determine the data type of x and print its value before moving forward.
+print(f"Data type of x: {type(x)}")
+print(f"Entered coordinate: {x}")
+
+dd = int(x[:2])        # First two character are degrees
+mm = int(x[2:4])       # Next two characters are minutes
+ss = int(x[4:])        # Last two characters are seconds
+
+# Convert to decimal degrees
+decimal_degrees = dd + (mm / 60) + (ss / 3600)
+
+#  Round the result to two decimal places
+decimal_degrees_rounded = round(decimal_degrees, 3)
+
+# Print the final result
+print(f"Final result is: {decimal_degrees_rounded} degrees")
